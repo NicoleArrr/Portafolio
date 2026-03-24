b5753b99c8bf0694748ce18a1c93066317d378bb
NicoleArias <nicoledayanaariaspinzon@gmail.com>
2026-03-24 18:02:30 -0500

    feat :sparkles: redirect page added

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+// Redirigir a página
+document.getElementById('startButton').addEventListener('click', function() {
+    window.location.href = 'https://nicolearrr.github.io/Portafolio/pages/pagina';
+});
